1f03619ca81f27954554d7aa86891c598480ee89
Mako9675 <Mako9675@users.noreply.github.com>
2023-03-05 10:28:32 +0000

    [add] モデルにバリデーションと紐づけ関係追記

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    has_one_attached :post_image
+    belongs_to :user
+    
+    has_many :favorites, dependent: :destroy
+    has_many :comments, dependent: :destroy
+    
+    validates :title, presence: true
+    validates :material, presence: true
+    validates :body, presence: true
+    validates :post_image, presence: false
+    
+    def get_post_image(width, height)
+      unless post_image.attached?
+        file_path = Rails.root.join('app/assets/images/no_image.jpg')
+        post_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpg')
+      end
+      post_image.variant(resize_to_limit: [width, height]).processed
+    end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :posts, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  validates :name, length: { minimum:2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+  validates :profile_image, presence: false
+  validates :email, uniqueness: true
+  
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
