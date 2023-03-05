346c5799d0764ab01caeef6d26c5842d60ad35c2
Mako9675 <Mako9675@users.noreply.github.com>
2023-03-05 07:08:59 +0000

    [add] コントローラー/モデル/ビュー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,51 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :admins
+  
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+    get 'users/update'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/create'
+    get 'genres/edit'
+    get 'genres/update'
+  end
+  
+  namespace :public do
+    get 'searches/search'
+  end
+  namespace :public do
+    get 'comments/create'
+    get 'comments/destroy'
+  end
+  namespace :public do
+    get 'favorites/create'
+    get 'favorites/destroy'
+  end
+  namespace :public do
+    get 'posts/new'
+    get 'posts/create'
+    get 'posts/index'
+    get 'posts/show'
+  end
+
+  
+  namespace :public do
+    get 'users/show'
+    get 'users/edit'
+    get 'users/update'
+    get 'users/unsubscribe'
+    get 'users/withdrawal'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
