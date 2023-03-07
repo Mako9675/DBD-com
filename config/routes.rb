d5e38ae3b5f5d4508da701a8286c75089a60b62c
Mako9675 <Mako9675@users.noreply.github.com>
2023-03-07 05:04:15 +0000

    [add] ログイン用のルーティング/View下準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :admins
+  devise_for :users, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
   
   root to: 'public/homes#top'
   
